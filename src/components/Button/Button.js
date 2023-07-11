79ab4934b31012d0e3972b45fcae697540bed275
Laydianne Costa <100539644+Laydiannenaira@users.noreply.github.com>
2023-07-11 10:09:54 -0300

    Add component Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,17 +1,23 @@
 import ButtonBS from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
 
-export const Button = ({ loading, variant, disabled }) => {
+export const Button = ({ loading, variant, disabled, label, loadingLabel, onClick }) => {
   return (
-      <ButtonBS variant="variant" disabled={disabled} >
-        <Spinner
-          as="span"
-          animation="border"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-      {/* <span className="visually-hidden">Loading...</span> */}
+    <ButtonBS variant={variant} disabled={disabled} onClick={onClick}>
+      {loading && (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>{' '}
+        </>
+      )}
+      {loading ? loadingLabel : label}
     </ButtonBS>
   )
 }
